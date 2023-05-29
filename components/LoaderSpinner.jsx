d4e0ad1b8084b5ca0e6cf18990ee885885d8f853
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-05-29 16:05:48 +0500

    loader position readjusted

--- components/LoaderSpinner.jsx
+++ components/LoaderSpinner.jsx
@@ -4,7 +4,7 @@ import { InfinitySpin } from "react-loader-spinner";
 
 const LoaderSpinner = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-100 absolute top-0 left-0">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <InfinitySpin width="200" color="#1d4ed8" />
