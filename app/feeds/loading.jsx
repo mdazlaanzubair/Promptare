636d5445cf03a7af80c7678dbf16fa06b1829f1a
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-05-29 13:12:40 +0500

    loaders added for each route

---
+++ app/feeds/loading.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { InfinitySpin } from "react-loader-spinner";
+
+function loading() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <InfinitySpin width="200" color="#1d4ed8" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default loading;
